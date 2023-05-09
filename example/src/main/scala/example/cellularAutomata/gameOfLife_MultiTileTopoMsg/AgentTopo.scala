06cefc542c80ab8910c334665e4bd3f180ea4802
Monstertail <769845882@qq.com>
2023-05-09 19:52:41 +0800

    clean the code for AgentTopo

--- example/src/main/scala/example/cellularAutomata/gameOfLife_MultiTileTopoMsg/AgentTopo.scala
+++ example/src/main/scala/example/cellularAutomata/gameOfLife_MultiTileTopoMsg/AgentTopo.scala
@@ -7,17 +7,12 @@ import meta.runtime.{Actor, Message}
 class AgentTopo[T]{
   //To check, topo requires another type signature
   var Atopo:IndexedSeq[Actor]= _
-  //TO DO : Modify it to a map
-//  var Ctopo:IndexedSeq[AgentTopo[T]]=_
 
-//  def topo():IndexedSeq[AgentTopo[T]]= _
 
   def tbs(Sid:T,Did:T) : TopoMsg = ???
   def processMessage[V](m:Message,c:AgentTopo[T]):V = ???
   def updateComponent[U](buffer:Iterable[(Int,Vector[U])],perCellAct:(U,Iterable[U])=>U):Unit = ???
 
-//  def updateMessage[V](c:AgentTopo[T] ,buffer:Iterable[V]):AgentTopo[T] = ???
 
-//  def step(c:AgentTopo[T]):AgentTopo[T] = ???
 
 }
